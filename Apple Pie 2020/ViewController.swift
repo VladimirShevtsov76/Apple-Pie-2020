03270d8184447f4046a2bd8b34b2abdcf96e92b6
Vladimir Shevtsov <work2976@gmail.com>
2021-02-24 10:44:30 +0300

    Initial Commit

---
+++ Apple Pie 2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Pie 2020
+//
+//  Created by Vladimir Shevtsov on 24.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
